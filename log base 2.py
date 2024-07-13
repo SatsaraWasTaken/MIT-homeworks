edb220d018c19fe532ad532202a22077299e9d14
SatsaraWasTaken <86369548+SatsaraWasTaken@users.noreply.github.com>
2024-07-13 12:46:34 +0300

    Add files via upload

---
+++ log base 2.py
@@ -0,0 +1,11 @@
+# Problem Set 0
+# Name: Mustafa Sabri Derbent
+# Collaborators: -
+# Time Spent: 3 minutes
+
+import math
+
+x = int(input('Enter a x value: '))
+y = int(input('Enter a y value: '))
+print(x**y)
+print(math.log(x,2))
