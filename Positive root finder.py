135aced7a1111e0d1a7b2dad8f7165391e51177f
SatsaraWasTaken <86369548+SatsaraWasTaken@users.noreply.github.com>
2024-07-13 13:56:59 +0300

    Added "\" to the print segments

--- Positive root finder.py
+++ Positive root finder.py
@@ -3,9 +3,9 @@
 # Collaborators: -
 # Time Spent: 27 minutes
 
-a = 34
-b = 68
-c = -510
+a = 1
+b = -5
+c = 6
 root_1 = (-b + (b**2 -4*a*c)**0.5)/2*a
 root_2 = (-b - (b**2 -4*a*c)**0.5)/2*a
 
@@ -16,10 +16,10 @@ def is_positive(a):
         return False
     
 if is_positive(root_1) and is_positive(root_2) :
-    print(f'Positive roots of the equation are: {root_1} and {root_2}')
+    print(f'Positive roots of the equation are:\n{root_1} and {root_2}')
 elif root_1 > root_2 and is_positive(root_1):
-    print(root_1)
+    print(f'Positive root of the equation is:\n{root_1}')
 elif root_2 > root_1 and is_positive(root_2):
-    print(root_2)
+    print(f'Positive root of the equation is:\n{root_2}')
 else:
     print('There are no positive roots of the equation')
