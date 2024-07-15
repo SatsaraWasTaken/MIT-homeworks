53044ba29df6dd30bdf92147e6e8f959dca0dff6
SatsaraWasTaken <86369548+SatsaraWasTaken@users.noreply.github.com>
2024-07-15 18:08:16 +0300

    Number State Detector

---
+++ Number State Detector.py
@@ -0,0 +1,13 @@
+# Finger Exercise 2
+# Name: Mustafa Sabri Derbent
+# Collaborators: -
+# Time Spent: 3 minutes
+
+number = -9
+
+if number > 0:
+    print(f'{number} is positive')
+elif number < 0:
+    print(f'{number} is negative')
+else:
+    print(f'{number} is equal to zero')
