dc3ac6a79656acb0e15b4d2c959fb788c6f3de81
SatsaraWasTaken <86369548+SatsaraWasTaken@users.noreply.github.com>
2024-07-15 18:51:00 +0300

    Even-index string displayer

---
+++ Even-index string displayer.py
@@ -0,0 +1,12 @@
+# Finger Exercise 5
+# Name: Mustafa Sabri Derbent
+# Collaborators: -
+# Time Spent: 5 minutes 04 seconds
+
+
+my_string = str(input('Enter a word or a sentence: '))
+i = 0
+while i in range(len(my_string)):
+    print(my_string[i])
+    i += 2
+    
