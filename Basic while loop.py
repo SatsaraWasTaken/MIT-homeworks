b772c738475090f5e63f4384f053d7e6c3730cd2
SatsaraWasTaken <86369548+SatsaraWasTaken@users.noreply.github.com>
2024-07-15 18:17:04 +0300

    Basic While Loop

---
+++ Basic while loop.py
@@ -0,0 +1,12 @@
+# Finger Exercise 3
+# Name: Mustafa Sabri Derbent
+# Collaborators: -
+# Time Spent: 3 minutes 48 seconds
+
+n = int(input('Enter a whole number: '))
+s = 'Hello World!'
+i = 0
+while i < n :
+    print(s)
+    i += 1
+    
